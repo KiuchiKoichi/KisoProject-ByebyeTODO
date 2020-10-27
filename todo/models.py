c0ad7c110b2d2d181fdfe5dc97be2046eeffe9d7
krnmk <karino-mako34@g.ecc.u-tokyo.ac.jp>
2020-10-27 23:17:49 +0900

    todo修正

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import Group
 # Create your models here.
 
 
 class Task(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     task_text = models.CharField(max_length=200)
     deadline = models.DateTimeField()
 
--- todo/urls.py
+++ todo/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'todo'
 urlpatterns = [
-    path('', views.todolist, name='todolist'),
-    path('add/', views.add, name='add'),
-    path('delete_modify/', views.delete_modify, name='delete_modify')
+    path('<int:group_id>/', views.index, name='index'),
+    path('<int:group_id>/add/', views.add_task, name='add'),
+    path('<int:group_id>/delete/', views.delete_task, name='delete'),
+    path('<int:group_id>/modify/', views.modify_task, name='modify'),
 ]
